1802e6361575b3ccabe5c975a66c36fdbcd66f4e
MaximeBouton <maxim.bouton@gmail.com>
2019-09-16 18:26:25 -0700

    better testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,34 +1,38 @@
 using TabularTDLearning
 using POMDPs
 using POMDPModels
+using POMDPTesting
 using Test
 
 mdp = SimpleGridWorld()
 
 
 @testset "qlearning" begin
-    solver = QLearningSolver(mdp, learning_rate=0.1, n_episodes=5000, max_episode_length=50, eval_every=50, n_eval_traj=100)
+    solver = QLearningSolver(mdp, learning_rate=0.1, n_episodes=500, max_episode_length=50, eval_every=50, n_eval_traj=100)
 
     println("Test QLearning requirements: ")
     @requirements_info solver mdp
 
-    policy = solve(solver, mdp)
+    r = test_solver(solver, mdp, max_steps = 100)
+    @test r > 0.0
 end
 
 @testset "sarsa" begin
-    solver = SARSASolver(mdp, learning_rate=0.1, n_episodes=5000, max_episode_length=50, eval_every=50, n_eval_traj=100)
+    solver = SARSASolver(mdp, learning_rate=0.1, n_episodes=500, max_episode_length=50, eval_every=50, n_eval_traj=100)
 
     println("Test SARSA requirements: ")
     @requirements_info solver mdp
 
-    policy = solve(solver, mdp)
+    r = test_solver(solver, mdp, max_steps = 100)
+    @test r > 0.0
 end
 
 @testset "sarsa λ" begin
-    solver = SARSALambdaSolver(mdp, learning_rate=0.1, lambda=0.9, n_episodes=5000, max_episode_length=50, eval_every=50, n_eval_traj=100)
+    solver = SARSALambdaSolver(mdp, learning_rate=0.1, lambda=0.9, n_episodes=500, max_episode_length=50, eval_every=50, n_eval_traj=100)
 
     println("Test SARSALambda requirements: ")
     @requirements_info solver mdp
 
-    policy = solve(solver, mdp)
+    r = test_solver(solver, mdp, max_steps = 100)
+    @test r > 0.0
 end
